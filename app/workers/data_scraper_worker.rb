56cac9840e2d56bb45cff2f7d0eb26a420782f90
Noi <noi@localhost.localdomain>
2015-10-20 20:13:06 +0700

    Better version

--- app/workers/data_scraper_worker.rb
+++ app/workers/data_scraper_worker.rb
@@ -7,38 +7,29 @@ class DataScraperWorker
     page = m.get('http://hipflat.github.io/')
     html = Nokogiri::HTML(page.body)
     html.css('table.building tbody').each {|b|
-      rows = b.css('tr')
-      name = rows[0].css('td').text.strip
+      building = nil
 
-      building = Building.where(name: name).first
-      if building.blank?
-        building = Building.new
-        building.name = name
-      end
+      b.css('tr').each { |r|
+          field = r.css('th').text
+          value = r.css('td').text.strip
+          field_name = field.gsub(/ /, "_").downcase
+          if "name" == field_name
+            building = Building.where(name: value).first
+            if building.nil?
+              building = Building.new
+              building.name = value
+            end
+          else
+            field_value = case field_name
+              when "median_sale_price", "median_rent_price", "distance_to_the_nearest_public_transit_station"
+                value.to_f
+              else
+                value
+            end
+            building[field_name] = field_value
+          end
+      }
 
-      if 3 == rows.length
-        nearest = rows[1].css('td').text.strip
-        distance = rows[2].css('td').text.to_f
-      elsif 4 == rows.length
-        field = rows[1].css('th').text
-        value = rows[1].css('td').text.strip
-        field_name = field.gsub(/ /, "_").downcase
-        if "median_sale_price" == field_name
-          building.median_sale_price = value.to_f
-        elsif "median_rent_price" == field_name
-          building.median_rent_price = value.to_f
-        end
-        nearest = rows[2].css('td').text.strip
-        distance = rows[3].css('td').text.to_f
-      else 
-        building.median_sale_price = rows[1].css('td').text.strip.to_f
-        building.median_rent_price = rows[2].css('td').text.strip.to_f
-        nearest = rows[3].css('td').text.strip
-        distance = rows[4].css('td').text.to_f
-      end
-
-      building.nearest_public_transit_station = nearest
-      building.distance_to_the_nearest_public_transit_station = distance
       building.save
     }
   end
